712b849d24a3519e8c9b5cc84b64fcb5834affb5
Văn Lộc Nguyễn <locnvgcd18350@fpt.edu.vn>
2021-01-31 15:24:16 +0700

    Add project files.

---
+++ BlogMVC/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BlogMVC.Models;
+using System.Data.Entity;
+
+namespace BlogMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        //Get: Post
+        public ActionResult Index()
+        {
+            var posts = db.Posts.Include(p => p.Author).OrderByDescending(p => p.Date).Take(3);
+            return View(posts.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
